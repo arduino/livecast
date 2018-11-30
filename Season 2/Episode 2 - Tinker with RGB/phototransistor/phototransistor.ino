6409a92ea03914c5e215739f70447f3b9024cea3
Karl Söderby <35461661+karlsoderby@users.noreply.github.com>
2018-11-30 11:20:24 +0100

    Update phototransistor.ino

--- Season 2/Episode 2 - Tinker with RGB/phototransistor/phototransistor.ino
+++ Season 2/Episode 2 - Tinker with RGB/phototransistor/phototransistor.ino
@@ -1,3 +1,12 @@
+/*
+    Control a RGB LED with a phototransistor
+    
+    Map the values of a phototransistors to control
+    an RGB LED, using multiple if statements
+    
+    (c) Karl, Josefine and Davide for Arduino, 2018
+*/
+
 
 int photoPin = A0;
 int photoValue = 0;
@@ -23,34 +32,34 @@ void loop() {
   
   //if we cover the photoresistor, the RGB will give a strange color
   if (photoValue >= 1 && photoValue <= 5) {
-    analogWrite(redPin, (255, 255, 255));
-    analogWrite(greenPin, (127, 127, 127));
-    analogWrite(bluePin, (80 , 80, 80));
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 127);
+    analogWrite(bluePin, 80);
   }
   //if our hand gets close enough, the RGB will turn blue
   if (photoValue >= 6 && photoValue <= 12) {
-    analogWrite(redPin, (0, 0, 0));
-    analogWrite(greenPin, (0, 0, 0));
-    analogWrite(bluePin, (255, 255, 255));
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 255);
   }
   //if our hand is semi-far away, the RGB will turn green
   if (photoValue >= 13 && photoValue <= 20) {
-    analogWrite(redPin, (0, 0, 0));
-    analogWrite(greenPin, (255, 255, 255));
-    analogWrite(bluePin, (0, 0, 0));
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 0);
   }
 
   //if we do nothing, the RGB will stay red
   if (photoValue >= 21 && photoValue <= 50) {
-    analogWrite(redPin, (255, 255, 255));
-    analogWrite(greenPin, (0, 0, 0));
-    analogWrite(bluePin, (0, 0, 0));
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
   }
   //if we use a flashlight on the photoresistor, the RGB will turn purple
   if (photoValue >= 50) {
-    analogWrite(redPin, (255, 255, 255));
-    analogWrite(greenPin, (0, 0, 0));
-    analogWrite(bluePin, (255, 255, 255));
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 255);
   }
   //we use a delay so the serial port doesn't get too busy
 delay(100);
